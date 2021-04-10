d9d2240f759a0af8f068066fba26603d435d45b3
yeawonbong <ybong.develop@gmail.com>
2021-04-10 21:39:02 +0900

    works but wrong

--- BJ1406-에디터.c
+++ BJ1406-에디터.c
@@ -2,32 +2,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char	*ft_strdup(const char *str)
+{
+	char	*s;
+	int		i;
+
+	i = 0;
+	if (!(s = (char*)malloc(strlen(str) + 1)))
+		return (0);
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
 int main()
 {
 	char	str[100001];
-	char	*cmdlist;
-	char 	*ptr;
 	int		cmdnum;
-	int		i;
+	char 	*ptr;
+	char	cmd[3];
+	int		j=0;
+	char	*temp;
 
 	scanf("%s", str);
-	cmdlist = (char*)malloc(scanf("%d", &cmdnum) + 1);
-
-	/*idn()
+	ptr = &str[strlen(str)];
+	scanf("%d", &cmdnum);
+	for (int i = 0; i <= cmdnum; i++)
 	{
-		if ()
-	}*/
-
-/*
-	for (i = 0; i < cmdnum; i++)
-	{
-		
-		scanf("%s", &str[i++]);
+		gets(cmd);
+		if (*cmd == 'L' && ptr != str)
+			ptr--;
+		if (*cmd == 'D' && *ptr != '\0')
+			ptr++;
+		if (*cmd == 'B' && ptr != str)
+		{
+			temp = strdup(ptr--);
+			j = 0;
+			while (temp[j])
+			{
+				ptr[j] = temp[j];
+				j++;
+			}
+			ptr[j] = '\0';
+		}
+		if (*cmd == 'P')
+		{
+			temp = strdup(ptr);
+			*ptr++ = cmd[2];
+			j = 0;
+			while (temp[j])
+			{
+				ptr[j] = temp[j];
+				j++;
+			}
+			ptr[j] = '\0';
+		}
 	}
-*/
-
-	printf("%s\n", str);
-	printf("%d\n", cmdnum);
-
+	printf("%s", str);
 	return 0;
 }
